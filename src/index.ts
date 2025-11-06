057f7ac82b28f130554e5292e9a9d59f8e0261a8
Rupam <rupam@brainpan.co>
2025-11-06 18:27:40 +0530

    test commit

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Load env variables
 const PORT: any = process.env.PORT || 3000;
-const APP_NAME = process.env.APP_NAME || "My Node App";
+const APP_NAME = process.env.APP_NAME || "My Node App with CICD";
 
 // Dummy route
 app.get("/", (req: Request, res: Response) => {
