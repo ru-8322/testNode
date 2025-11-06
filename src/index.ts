af013bb6dadaa985addb62f43111991081715c20
Rupam <rupam@brainpan.co>
2025-11-06 16:51:17 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Load env variables
+const PORT: any = process.env.PORT || 3000;
+const APP_NAME = process.env.APP_NAME || "My App";
+
+// Dummy route
+app.get("/", (req: Request, res: Response) => {
+    res.json({
+        message: `🚀 Hello from ${APP_NAME}! Rupam`,
+        port: PORT,
+        time: new Date().toISOString(),
+    });
+});
+
+const HOST = '0.0.0.0';
+
+// Start server
+app.listen(PORT, HOST, () => {
+    console.log(`✅ My App running at http://${HOST}:${PORT}`);
+});
