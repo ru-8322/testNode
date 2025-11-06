fbed6df978eab9bf20f75ed0eaf089cc5bfad441
Rupam <rupam@brainpan.co>
2025-11-06 17:52:27 +0530

    Trigger workflow

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 
 // Load env variables
 const PORT: any = process.env.PORT || 3000;
-const APP_NAME = process.env.APP_NAME || "My App";
+const APP_NAME = process.env.APP_NAME || "My Node App";
 
 // Dummy route
 app.get("/", (req: Request, res: Response) => {
